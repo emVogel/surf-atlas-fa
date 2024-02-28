1181ac487310c1002a01da8e15c7f7355f1e8a9c
emVogel <michavogel80@gmail.com>
2024-02-28 01:32:40 +0100

    fix leaflet bug MapContainer is already initialized

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,28 +1,57 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Popup, CircleMarker } from "react-leaflet";
 import "./Map.scss";
 
-import { Feature, Geo } from "../../shared/model/spot.interface";
-import { useQuery } from "@tanstack/react-query";
-import { spotQuery } from "../../shared/route-loaders/map-data-loader";
+import { Feature } from "../../shared/model/spot.interface";
+import { useRef, useLayoutEffect, useState } from "react";
 
 interface MapProps {
   features: Feature[];
+  selectedSpot: string;
 }
 
 function Map(props: MapProps) {
+  const map = useRef(null);
+
+  const [unmountMap, setunmountMap] = useState(false);
+  //to prevent map re-initialization
+  useLayoutEffect(() => {
+    setunmountMap(false);
+    return () => {
+      setunmountMap(true);
+    };
+  }, []);
+
+  if (unmountMap) {
+    return null;
+  }
+
   return (
-    <MapContainer center={[42.505, -10]} zoom={8} scrollWheelZoom={true}>
+    <MapContainer
+      center={[42.705, -7.5]}
+      zoom={8}
+      scrollWheelZoom={true}
+      ref={map}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {props.features.map((feature: Feature) => (
-        <Marker
+        <CircleMarker
           key={feature?.properties.id}
-          position={[
+          center={[
             feature.geometry.coordinates[1],
             feature.geometry.coordinates[0],
           ]}
+          radius={5}
+          pathOptions={{
+            color:
+              props.selectedSpot === feature.properties.id ? "red" : "#777777",
+            fill: true,
+            fillOpacity: 1,
+            fillColor:
+              props.selectedSpot === feature.properties.id ? "red" : "#00000",
+          }}
         >
           <Popup>
             <ul>
@@ -30,7 +59,7 @@ function Map(props: MapProps) {
               <li>2</li>
             </ul>
           </Popup>
-        </Marker>
+        </CircleMarker>
       ))}
     </MapContainer>
   );
