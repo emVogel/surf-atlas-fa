927d57220e50cfc258566697209e48d534ddeb9f
emVogel <michavogel80@gmail.com>
2024-03-09 15:33:18 +0100

    cleanup

--- src/Pages/Atlas/Atlas.tsx
+++ src/Pages/Atlas/Atlas.tsx
@@ -1,15 +1,8 @@
-import { Outlet, useLoaderData, Await, defer } from "react-router-dom";
-import Map from "../../components/Map/Map";
-import { Feature, HttpResponseStatus } from "../../shared/model/spot.interface";
-import { useState } from "react";
-import Spinner from "../../shared/components/spinner/Spinner";
+import { Outlet } from "react-router-dom";
+
 import { Suspense } from "react";
-import LoaderError from "../../shared/components/LoaderError/LoaderError";
-import { useQuery } from "@tanstack/react-query";
-import { spotQuery } from "../../shared/route-loaders/map-data-loader";
-import Spots from "../../components/Spots/Spots";
+
 import { Skeleton } from "../../components/skeleton/Skeleton";
-import SpotsView from "../../components/Spots/Spots";
 
 function Atlas() {
   return (
